881b2331b6075b3739d162b3d9d486ac014dff6a
Radoslav Georgiev <radoslavasz@abv.bg>
2023-08-02 16:35:45 +0300

    Project

--- src/main/java/com/example/configuration/AppBeanConfig.java
+++ src/main/java/com/example/configuration/AppBeanConfig.java
@@ -59,7 +59,7 @@ public class AppBeanConfig {
         mailSender.setHost("smtp-relay.brevo.com");
         mailSender.setPort(587);
         mailSender.setUsername("pawfinder.team@gmail.com");
-        mailSender.setPassword("ASK ALEX FOR API");
+        mailSender.setPassword("API");
 
         return mailSender;
     }
